c93f451795245b04c980cd71f54d80555d06ee56
Ryan Hamilton <ryhamz@gmail.com>
2018-02-13 22:08:32 -0500

    fixed comments create table, added comment db functions

--- db.py
+++ db.py
@@ -27,18 +27,21 @@ def initialize_db():
                 owner_id TEXT
             );
             """)
-        c.execute("""
+
+        conn.commit()
+    except sqlite3.OperationalError:
+        print "initialization error"
+    c.execute("""
             CREATE TABLE comments (
                 comment_id TEXT PRIMARY KEY,
                 commenter_id TEXT,
                 commenter_username TEXT,
                 message TEXT,
-                FOREIGN KEY(project_id) REFERENCES projects(project_id)
+                p_id TEXT,
+                FOREIGN KEY(p_id) REFERENCES projects(project_id)
             );
             """)
-        conn.commit()
-    except sqlite3.OperationalError:
-        pass
+    conn.commit()
 
 
 def get_num_projects():
@@ -48,6 +51,34 @@ def get_num_projects():
     c.execute("SELECT COUNT(project_id) FROM projects")
     return c.fetchone()[0]
 
+
+def get_comments_for_project(project_id):
+    """
+    Returns all comments for the given project_id
+    """
+    c.execute('''SELECT comment_id, commenter_id,
+                 commenter_username, message FROM comments
+                 WHERE p_id = ?''', (project_id,))
+    return c.fetchall()
+
+def delete_comments_for_project(project_id):
+    """
+    Deletes all comments for the given project_id
+    """
+    c.execute('''DELETE FROM comments
+                 WHERE p_id = ?''', (project_id,))
+    conn.commit()
+
+
+def add_comment(comment_id, commenter_id, commenter_username, message, project_id):
+    """
+    Inserts a new comment
+    """
+    c.execute(''' INSERT INTO comments(comment_id, commenter_id, commenter_username, message, p_id)
+        VALUES(?, ?, ?, ?, ?) ''', (comment_id, commenter_id, commenter_username, message, project_id))
+    conn.commit()
+
+
 def get_project_by_id(project_id):
     """
     Returns project with given id
@@ -56,6 +87,7 @@ def get_project_by_id(project_id):
         WHERE project_id = ?''', (project_id,))
     return c.fetchone()
 
+
 def add_project(project_id, project_name, owner_username, owner_id):
     """
     Inserts a new project entry
@@ -63,6 +95,8 @@ def add_project(project_id, project_name, owner_username, owner_id):
     c.execute(''' INSERT INTO projects(project_id, project_name, owner_username, owner_id)
         VALUES(?, ?, ?, ?) ''', (project_id, project_name, owner_username, owner_id))
     conn.commit()
+
+
 def delete_project(project_id):
     """
     Deletes the given project entry
@@ -71,9 +105,25 @@ def delete_project(project_id):
         WHERE project_id = ? ''', (project_id,))
     conn.commit()
 
+def delete_owners_project(project_id, owner_id):
+    """
+    Deletes the given project entry, but check that the owner
+    id matches.
+    """
+    c.execute(''' DELETE FROM projects
+        WHERE project_id = ? and owner_id = ?''', (project_id, owner_id))
+    result = c.rowcount
+    conn.commit()
+    if rowcount == 0:
+        return False
+    return True
+
+
 def delete_all_projects():
     """
     Testing function, used to clean db
     """
     c.execute('DELETE FROM projects')
     conn.commit()
+
+
